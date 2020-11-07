fa4aac74a203d3ad669790c971e0537a563858f9
Daveeed3 <dsan2@calstatela.edu>
2020-11-07 11:39:20 -0800

    Added the 3-1

--- src/Cracking.java
+++ src/Cracking.java
@@ -15,61 +15,28 @@ key used for its encryption.
  */
 public class Cracking {
 	
+	static String string = "CRYPTOGRAPHY";
 	static String msg1 = "1011011001111001001011101111110000111110100000000001110111010001111011111101101100010011000000101101011010101000101111100011101011010111100011101001010111101100101110000010010101110001110111011111010101010100001100011000011010101111011111010011110111001001011100101101001000011011111011000010010001011101100011011110000000110010111111010000011100011111111000010111010100001100001010011001010101010000110101101111111010010110001001000001111000000011110000011110110010010101010100001000011010000100011010101100000010111000000010101110100001000111010010010101110111010010111100011111010101111011101111000101001010001101100101100111001110111001100101100011111001100000110100001001100010000100011100000000001001010011101011100101000111011100010001111101011111100000010111110101010000000100110110111111000000111110111010100110000010110000111010001111000101011111101011101101010010100010111100011100000001010101110111111101101100101010011100111011110101011011";
 	static String msg2 = "00011111100111111110011111101100111000000011001011110010101010110001011101001101000000110011010111111110000000001010111111000001010010111001111001010101100000110111100011111101011100100100010101000011001100101000000101111011000010011010111100010001001000100001111100100000001000000001101101000000001010111010000001000010011100101111001101111011001001010001100010100000";
-	
+
 	
 	public static void main(String[] args) {		
-		byte[] rawkey = {0,1,1,1,0, 0,1,1,0,1};
-		ArrayList<byte[]> encryptedLetters = new ArrayList<byte[]>();
-
-		//CRYPTOGRAPHY
-		// Each letter is a 2^5 = 32
-		// Or what if it is CRYPTO GRAPHY
-//		String string = "CRYPTOGRAPHY";
-//		for (int i=0; i<string.length(); i++) {
-//			// Turn into CASCII
-//			byte[] casciiLetter = cascii.Convert(string.charAt(i) + "");
-//
-//			//Encrypt the letter
-//			byte[] encryptedLetter = sdes.Encrypt(rawkey, casciiLetter);
-//			encryptedLetters.add(encryptedLetter);
-//		}
-		
-		
-		
-//		int start = 600;
-//		int stop = 800;
-//		for (int co=start; co<stop; co++) {
-//			System.out.println(currentIter);
-//			byte[] rawkeyMsg1 = stringTo10BitArray(currentIter);
-//			currentIter = addBit(currentIter);
-//			
-//			for (int i=0; i<msg1.length()/8; i++) {
-//				byte[] msg18bit = new byte[8];
-//				for (int j=0; j<msg18bit.length; j++) {
-//					if (j < 8) {
-//						msg18bit[j] = (byte)(msg1.charAt(j + (i*5)) -48);	
-//					} else {
-//						msg18bit[j] = 0;
-//					}
-//				}
-//				
-////				sdes.printArray(sdes.Decrypt(rawkey, msg18bit));
-//				System.out.print(cascii.Convert(sdes.subArray(sdes.Decrypt(rawkeyMsg1, msg18bit), 0, 4)) );
-//				
-//			}	
-//			System.out.println();
-//		}
-		
 		part1();
-//		part2();
-//		part3(); // Takes about a minute to complete
-		
-		
+		part2();
+		part3(); // Takes about a minute to complete		
 	}
 	
 	public static void part1() {
+		String rawkey = "0111001101";
+		
+		// Convert message to CASCII byte array
+		byte[] plaintext = CASCII.Convert(string);
+		
+		// Ecrypt the CASCII message with the key
+		byte[] ciphertext = SDES.Encrypt(toByteArray(rawkey), plaintext);
+		
+		System.out.println("Encrypted Message: ");
+		printArray(ciphertext);
 		
 	}
 	
@@ -167,4 +134,10 @@ public class Cracking {
 		return temp;
 	}
 
+	public static void printArray(byte[] array) {
+		for (int i=0; i< array.length; i++) {
+			System.out.print(array[i]);
+		}
+		System.out.println();
+	}
 }
